bf07bc8f067343a199faa5cbb06ae0dd61b18df2
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-06-13 10:05:12 +0100

    Add more style to about section

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(window).on('load', function() {
   alert('window is fully loaded');
 });
 */
-$(window).on('load', function() {
+$(window).on('load', function () {
   $('#status').fadeOut();
   $('#preloader').delay(500).fadeOut('slow');
 });
